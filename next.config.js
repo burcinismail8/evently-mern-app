5104b1059f5e3f83c6985546ecbc50bdd5c1cef7
dev9114537 <dev@activepolitics.bg>
2024-02-24 13:28:16 +0200

    Created Event Details page

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@ const nextConfig = {
   experimental: {
     optimizePackageImports: ["@mantine/core", "@mantine/hooks"],
   },
+  images: {
+    domains: ["utfs.io"],
+    remotePatterns: [{ protocol: "https", hostname: "utfs.io" }],
+  },
 };
 
 module.exports = nextConfig;
