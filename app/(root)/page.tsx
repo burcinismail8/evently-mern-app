1fb363833659525a0b4fdfa8ac73f8ba08a1fb3c
dev9114537 <dev@activepolitics.bg>
2024-02-24 14:07:51 +0200

    Created Collection component

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,16 @@
 import Collection from "@/components/shared/Collection";
 import { Button } from "@/components/ui/button";
+import { getAllEvents } from "@/lib/actions/event.actions";
 import Image from "next/image";
 import Link from "next/link";
 
-export default function Home() {
+const Home = async () => {
+  const events = await getAllEvents({
+    query: "",
+    category: "",
+    page: 1,
+    limit: 6,
+  });
   return (
     <>
       <section className="bg-primary-50 bg-dotted-pattern bg-contain py-5 md:py-10">
@@ -39,8 +46,17 @@ export default function Home() {
         <div className="flex w-full flex-col gap-5 md:flex-row">
           Search CategoryFilter
         </div>
-        <Collection />
+        <Collection
+          data={events?.data}
+          emptyTitle="No Events Found"
+          emptyStateSubtext="Come back later"
+          collectionType="All_Events"
+          limit={6}
+          page={1}
+          totalPages={2}
+        />
       </section>
     </>
   );
-}
+};
+export default Home;
--- components/shared/Collection.tsx
+++ components/shared/Collection.tsx
@@ -1,7 +1,38 @@
+import { IEvent } from "@/lib/database/models/event.model";
 import React from "react";
-
-const Collection = () => {
-  return <div>Collection</div>;
+type CollectionProps = {
+  data: IEvent[];
+  emptyTitle: string;
+  emptyStateSubtext: string;
+  limit: number;
+  page: number | string;
+  totalPages?: number;
+  urlParamName?: string;
+  collectionType?: "Event_Organized" | "My_Tickets" | "All_Events";
+};
+const Collection = ({
+  data,
+  emptyTitle,
+  emptyStateSubtext,
+  page,
+  totalPages = 0,
+  collectionType,
+  urlParamName,
+}: CollectionProps) => {
+  return (
+    <>
+      {data.length > 0 ? (
+        <div className="flex flex-col items-center gap-10">
+          <ul className="grid w-fulll grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-3 xl:gap-10"></ul>
+        </div>
+      ) : (
+        <div className="flex flex-center wrapper min-h-[200px] w-full flex-col gap-3 rounded-[14px] bg-grey-50 py-28 text-center">
+          <h3 className="p-bold-20 md:h5-bold">{emptyTitle}</h3>
+          <p className="p-regular-14">{emptyStateSubtext}</p>
+        </div>
+      )}
+    </>
+  );
 };
 
 export default Collection;
